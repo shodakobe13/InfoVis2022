fc400e1ab8653f2ba9c1a2120a81c54d37e806ad
正田茉鈴 <marin@Marin-MacBook-2.local>
2022-05-07 16:57:19 +0900

    third commit4

--- W06/w06_task2.js
+++ W06/w06_task2.js
@@ -1,4 +1,4 @@
-d3.csv("https://vizlab-kobe-lecture.github.io/InfoVis2021/W04/data.csv")
+d3.csv("https://shodakobe13.github.io/InfoVis2022/W04/data.csv")
     .then( data => {
         data.forEach( d => { d.x = +d.x; d.y = +d.y; });
 
@@ -39,8 +39,19 @@ class ScatterPlot {
         self.chart = self.svg.append('g')
             .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
 
+
         self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+
+        self.title = self.svg.append("g")
+            .attr("transform", "translate(200,20)")
+            .append("text")
+            .attr("fill", "black")
+            .attr("x", 20)
+            .attr("y", 0)
+            .attr("font-size", "10pt")
+            .attr("font-weight", "bold")
+            .text("Task2");
 
         self.xscale = d3.scaleLinear()
             .range( [0, self.inner_width] );
@@ -59,6 +70,8 @@ class ScatterPlot {
 
         self.yaxis_group = self.chart.append('g')
             .attr('transform', `translate(0, 0)`);
+
+
     }
 
     update() {
@@ -69,7 +82,7 @@ class ScatterPlot {
         self.xscale.domain( [xmin, xmax] );
 
         const ymin = 0;
-        const ymax = d3.max( self.data, d => d.y ) +20;
+        const ymax = d3.max( self.data, d => d.y ) + 20;
         self.yscale.domain( [ymin, ymax] );
 
         self.render();
@@ -87,9 +100,28 @@ class ScatterPlot {
             .attr("r", d => d.r );
 
         self.xaxis_group
-            .call( self.xaxis );
+            .call( self.xaxis )
+            .append("text")
+            .attr("fill", "black")
+            .attr("x", 140)
+            .attr("y", 35)
+            .attr("text-anchor", "middle")
+            .attr("font-size", "10pt")
+            .attr("font-weight", "bold")
+            .text("x-label");
+
 
         self.yaxis_group
-            .call( self.yaxis );
+            .call( self.yaxis )
+            .append("text")
+            .attr("fill", "black")
+            .attr("text-anchor", "middle")
+            .attr("x", -90)
+            .attr("y", -40)
+            .attr("transform", "rotate(-90)")
+            .attr("text-anchor", "middle")
+            .attr("font-size", "10pt")
+            .attr("font-weight", "bold")
+            .text("y-label");
     }
 }
