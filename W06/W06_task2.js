c7e50e3f201efddb2b480ecf9c31c48960cf9e43
正田茉鈴 <marin@Marin-MacBook-2.local>
2022-05-07 16:47:48 +0900

    third commit3

--- W06/W06_task2.js
+++ W06/W06_task2.js
@@ -1,4 +1,4 @@
-d3.csv("https://shodakobe13.github.io/InfoVis2022/W04/data.csv")
+d3.csv("https://vizlab-kobe-lecture.github.io/InfoVis2022/W04/data.csv")
     .then( data => {
         data.forEach( d => { d.x = +d.x; d.y = +d.y; });
 
