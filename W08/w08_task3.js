f0450fa7c86b08b01039ec11781dc0b6b3a56f29
正田茉鈴 <marin@Marin-MacBook-2.local>
2022-05-16 23:57:35 +0900

    fourth commit2

--- W08/w08_task3.js
+++ W08/w08_task3.js
@@ -33,33 +33,22 @@ class PieChart {
 
         self.svg = d3.select( self.config.parent )
             .attr('width', self.config.width)
-            .attr('height', self.config.height);
+            .attr('height', self.config.height)
+            .append('g')
+            .attr('transform', `translate(${width/2}, ${height/2})`);
 
-        self.chart = self.svg.append('g')
-            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+        self.pie = d3.pie()
+            .value( d => d.value );
 
-        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
-        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+        self.radius = Math.min( width, height ) / 2;
 
-        self.xscale = d3.scaleLinear()
-            .range( [0, self.inner_width] );
+        self.arc = d3.arc()
+            .innerRadius(self.radius/3)
+            .outerRadius(self.radius);
 
-        self.yscale = d3.scaleBand()
-            .domain(self.data.map(d => d.label))
-            .range( [self.inner_height, 0] )
-            .paddingInner(0.1);
 
-        self.xaxis = d3.axisBottom( self.xscale )
-            .ticks(5)
-            .tickSizeOuter(0);
 
-        self.xaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, ${self.inner_height} )`);
 
-        self.yaxis = d3.axisLeft( self.yscale )
-            .tickSizeOuter(0);
-
-        self.yaxis_group = self.chart.append('g');
 
         self.title = self.svg.append("g")
             .attr("transform", "translate(150,20)")
@@ -69,7 +58,7 @@ class PieChart {
             .attr("y", 0)
             .attr("font-size", "10pt")
             .attr("font-weight", "bold")
-            .text("Task1");
+            .text("Task3");
     }
 
     update() {
@@ -84,20 +73,15 @@ class PieChart {
     render() {
         let self = this;
 
-        self.chart.selectAll("rect")
-            .data(self.data)
+        self.svg.selectAll('pie')
+            .data( self.pie(self.data) )
             .enter()
-            .append("rect")
-            .attr("x", 0 )
-            .attr("y", d => self.yscale( d.label ) )
-            .attr("width", d => self.xscale( d.value ) )
-            .attr("height", self.yscale.bandwidth());
+            .append('path')
+            .attr('d', self.arc)
+            .attr('fill', 'black')
+            .attr('stroke', 'white')
+            .style('stroke-width', '2px');
 
-        self.xaxis_group
-            .call( self.xaxis );
-
-        self.yaxis_group
-            .call( self.yaxis );
     }
 }
 
