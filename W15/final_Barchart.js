10412724f28c80ae527defd6c2d09ec837d0553f
正田茉鈴 <marin@Marin-MacBook-2.local>
2022-06-12 18:23:36 +0900

    final commit

--- W15/final_Barchart.js
+++ W15/final_Barchart.js
@@ -15,7 +15,7 @@ class BarChart {
     init() {
         let self = this;
 
-        self.svg = d3.select( self.config.parent )
+        self.svg = d3.select(self.config.parent)
             .attr('width', self.config.width)
             .attr('height', self.config.height);
 
@@ -25,34 +25,56 @@ class BarChart {
         self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
 
-        self.xscale = d3.scaleLinear()
-            .range( [0, self.inner_width] );
+        self.xscale = d3.scaleBand()
+            .range([0, self.inner_width])
+            .paddingInner(0.2)
+            .paddingOuter(0.1);
 
-        self.yscale = d3.scaleBand()
-            .domain(self.data.map(d => d.period))
-            .range( [self.inner_height, 0] )
-            .paddingInner(0.1);
+        self.yscale = d3.scaleLinear()
+            .range([self.inner_height, 0]);
 
-        self.xaxis = d3.axisBottom( self.xscale )
+        self.xaxis = d3.axisBottom(self.xscale)
+            .ticks(5)
+            .tickSizeOuter(0);
+
+        self.yaxis = d3.axisLeft(self.yscale)
             .ticks(5)
             .tickSizeOuter(0);
 
         self.xaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, ${self.inner_height} )`);
-
-        self.yaxis = d3.axisLeft( self.yscale )
-            .tickSizeOuter(0);
+            .attr('transform', `translate(0, ${self.inner_height})`);
 
         self.yaxis_group = self.chart.append('g');
 
+        const xlabel_space = 40;
+        self.svg.append('text')
+            .attr('x', self.config.width / 2)
+            .attr('y', self.inner_height + self.config.margin.top + xlabel_space)
+            .text( self.config.xlabel );
+
+        const ylabel_space = 50;
+        self.svg.append('text')
+            .attr('transform', `rotate(-90)`)
+            .attr('y', self.config.margin.left - ylabel_space)
+            .attr('x', -(self.config.height / 2))
+            .attr('text-anchor', 'middle')
+            .attr('dy', '1em')
+            .text( self.config.ylabel );
     }
 
     update() {
         let self = this;
-        const xmin = 0;
-        const xmax = d3.max( self.data, d => d.corona_number );
-        self.xscale.domain( [xmin, xmax] );
-        self.yscale.domain( self.data.map(d => d.period) )
+
+        self.xvalue = d => d.period;
+        self.yvalue = d => d.corona_number;
+
+        const items = self.data.map( self.xvalue );
+        self.xscale.domain(items);
+
+        const ymin = 0;
+        const ymax = d3.max( self.data, self.yvalue );
+        self.yscale.domain([ymin, ymax]);
+
         self.render();
     }
 
@@ -60,18 +82,18 @@ class BarChart {
         let self = this;
 
         self.chart.selectAll("rect")
-            .data(self.data)
-            .enter()
-            .append("rect")
-            .attr("x", 0 )
-            .attr("y", d => self.yscale( d.period ) )
-            .attr("width", d => self.xscale( d.corona_number ) )
-            .attr("height", self.yscale.bandwidth());
+            .data(self.aggregated_data)
+            .join("rect")
+            .attr("x", d => self.xscale( self.xvalue(d) ) )
+            .attr("y", d => self.yscale( self.yvalue(d) ) )
+            .attr("width", self.xscale.bandwidth())
+            .attr("height", d => self.inner_height - self.yscale( self.yvalue(d) ))
+            .attr("fill", d => self.config.cscale( self.cvalue(d) ));
 
         self.xaxis_group
-            .call( self.xaxis );
+            .call(self.xaxis);
 
         self.yaxis_group
-            .call( self.yaxis );
+            .call(self.yaxis);
     }
 }
